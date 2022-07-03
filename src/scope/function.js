1cc6df29d5d82edf06bb4f280edbaf74e8386a47
Santiago Salgado <jssalgadog@gmail.com>
2022-07-03 12:14:23 -0500

    class3_function_scope

---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+function greeting() {
+    let username = 'Ana'; // function
+    console.log(username);
+
+    if (username === 'Ana'){
+        console.log(`Hello ${username}!`);
+    }
+
+}
+
+greeting();
+console.log(username); // no funcionaría
