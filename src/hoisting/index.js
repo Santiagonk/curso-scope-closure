5ea449eedacdcbc5e98722000f728e7f178af32b
Santiago Salgado <jssalgadog@gmail.com>
2022-07-06 05:31:20 -0500

    class8_hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,16 @@
+
+var nameOfDog; //undefined lo eleva antes del console de manera "virtual"
+
+console.log(nameOfDog);
+var nameOfDog = 'Elmo';
+console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perriro es ${elmo}`);
+}
+
+var elmo = 'Elmito';
+
+nameOfDog();
